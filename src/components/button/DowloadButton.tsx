733f9c1d21286f8ece36c40998317a63753f7812
Hùng Nguyễn <duyhung1632002@gmail.com>
2025-07-22 11:34:39 +0700

    :zap: update footer

--- src/components/button/DowloadButton.tsx
+++ src/components/button/DowloadButton.tsx
@@ -7,10 +7,10 @@ interface DownloadButtonProps {
 }
 
 export default function DownloadButton({ text = "Download", href = "#" }: DownloadButtonProps) {
-
+    const { isMobile } = useViewport();
     return (
         <a
-            className={`rounded-3xl bg-gradient-to-r from-sky-600 to-sky-800 px-8 py-3 text-white transition hover:scale-110 hover:rotate-2 focus:ring-3 focus:outline-hidden max-h-12 min-w-32`}
+            className={`group relative inline-flex items-center overflow-hidden rounded-3xl px-8 py-3 bg-gradient-to-r from-sky-600 to-sky-900 text-white transition hover:scale-110 hover:rotate-2 focus:ring-3 focus:outline-hidden max-h-12 min-w-32`}
             href={href}
         >
             {text}
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -28,8 +28,8 @@ export function Footer() {
                         <h3 className="font-bold text-gray-900 mb-4">Hỗ trợ</h3>
                         <ul className="space-y-3 text-sm text-gray-600">
                             <li>Hotline: 096 292 7273</li>
+                            <li>Email: cskh@skyhome.net.vn</li>
                             <li>Câu hỏi thường gặp</li>
-                            <li>Nguyên tắc cộng đồng</li>
                             <li>Điều khoản sử dụng</li>
                         </ul>
                     </div>
@@ -48,16 +48,24 @@ export function Footer() {
                     <div>
                         <h3 className="font-bold text-gray-900 mb-4">KH cá nhân</h3>
                         <ul className="space-y-3 text-sm text-gray-600">
-                            <li>Dịch vụ</li>
-                            <li>Thông tin mới nhất</li>
+                            <li>Vệ sinh theo giờ</li>
+                            <li>Tổng vệ sinh</li>
+                            <li>Vệ sinh máy lạnh</li>
+                            <li>Vệ sinh điều hoà</li>
+                            <li>Vệ sinh máy nóng lạnh</li>
+                            <li>Vệ sinh máy năng lượng mặt trời</li>
                         </ul>
+
                     </div>
 
                     {/* KH doanh nghiệp */}
                     <div>
                         <h3 className="font-bold text-gray-900 mb-4">KH doanh nghiệp</h3>
                         <ul className="space-y-3 text-sm text-gray-600">
-                            <li>Ưu đãi</li>
+                            <li>Vệ sinh công nghiệp</li>
+                            <li>Vệ sinh văn phòng</li>
+                            <li>Vệ sinh homestay</li>
+                            <li>Vệ sinh khách sạn</li>
                         </ul>
                     </div>
 
@@ -67,7 +75,7 @@ export function Footer() {
                         <ul className="space-y-3 text-sm text-gray-600">
                             <li>Hướng dẫn đăng ký</li>
                             <li>Chương trình đào tạo</li>
-                            <li>Cộng đồng</li>
+                            <li>Cộng đồng SkyHome</li>
                         </ul>
                     </div>
                 </div>
@@ -81,20 +89,14 @@ export function Footer() {
                     <div>
                         <p className="text-sm text-gray-600 mb-2">Theo dõi các nền tảng</p>
                         <div className="flex space-x-4">
-                            <a href="#" className="w-10 h-8 bg-blue-600 rounded-full flex items-center justify-center text-white hover:bg-blue-700 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                                </svg>
+                            <a href="#" className="w-10 h-8 rounded-full flex items-center justify-center text-white ">
+                                <Image src="/icon/fb-icon.png" alt="Facebook" width={40} height={40} />
                             </a>
-                            <a href="#" className="w-10 h-8 bg-red-600 rounded-full flex items-center justify-center text-white hover:bg-red-700 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-                                </svg>
+                            <a href="#" className="w-10 h-8 rounded-full flex items-center justify-center text-white ">
+                                <Image src="/icon/yt-icon.png" alt="YouTube" width={40} height={40} />
                             </a>
-                            <a href="#" className="w-10 h-8 bg-gray-900 rounded-full flex items-center justify-center text-white hover:bg-gray-800 transition-colors">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.174-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.1.119.112.225.083.345-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.402.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.357-.629-2.748-1.378 0 0-.599 2.283-.744 2.845-.282 1.080-1.064 2.456-1.549 3.235C9.584 23.815 10.77 24.001 12.017 24.001c6.624 0 11.99-5.367 11.99-11.987C24.007 5.367 18.641.001 12.017.001z" />
-                                </svg>
+                            <a href="#" className="w-10 h-8 rounded-full flex items-center justify-center text-white ">
+                                <Image src="/icon/tiktok-icon.png" alt="TikTok" width={40} height={40} />
                             </a>
                         </div>
                     </div>
@@ -103,10 +105,9 @@ export function Footer() {
                 {/* Company Information */}
                 <div className="mt-12 pt-4 border-t border-gray-200">
                     <div className="text-sm text-gray-600">
-                        <p className="font-semibold mb-2">CÔNG TY TNHH THƯƠNG MẠI DỊCH VỤ SKYHOME</p>
-                        <p>Địa chỉ: 46 Cửu Long, phường 2, Tân Bình</p>
-                        <p>Mã số doanh nghiệp: 0318963348</p>
-                        <p>SĐT: 096 292 7273</p>
+                        <p className="font-semibold mb-2">CÔNG TY TNHH THƯƠNG MẠI DỊCH VỤ SKYHOME VIỆT NAM</p>
+                        <p>Địa chỉ: 46 Cửu Long, phường Tân Sơn Hoà, TP Hồ Chí Minh</p>
+                        <p>MST: 0318963348</p>
                     </div>
                 </div>
             </div>
